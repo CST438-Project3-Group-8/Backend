83b69f9cd4796257645ee8b09b5198ae9bba35ad
Aurelio-0-0 <victoperez@csumb.edu>
2026-04-22 17:53:15 -0700

    Created the StudyGroup entity

---
+++ src/main/java/com/studyhive/spring_boot_docker/StudyGroup.java
@@ -0,0 +1,87 @@
+package com.studyhive.spring_boot_docker;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "study_group")
+public class StudyGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+
+    @Column(name = "course_id")
+    private Long courseId;
+
+    @Column(name = "creator_id")
+    private String creatorId;
+    
+    private String location;
+    private String meetingMode;
+    private Integer maxMembers;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getMaxMembers() {
+        return maxMembers;
+    }
+
+    public void setMaxMembers(Integer maxMembers) {
+        this.maxMembers = maxMembers;
+    }
+
+    public String getMeetingMode() {
+        return meetingMode;
+    }
+
+    public void setMeetingMode(String meetingMode) {
+        this.meetingMode = meetingMode;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(String creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
