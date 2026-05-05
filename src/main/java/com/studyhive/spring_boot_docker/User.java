00d32e2769c8c9c88e210886fab0b38017103b10
Aurelio-0-0 <victoperez@csumb.edu>
2026-05-05 13:12:44 -0700

    removed duplicated important

--- src/main/java/com/studyhive/spring_boot_docker/User.java
+++ src/main/java/com/studyhive/spring_boot_docker/User.java
@@ -3,7 +3,6 @@ package com.studyhive.spring_boot_docker;
 import java.util.Objects;
 import jakarta.persistence.*;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
