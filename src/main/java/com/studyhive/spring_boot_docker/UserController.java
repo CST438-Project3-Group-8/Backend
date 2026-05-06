194d2f1a7066fe44d2704614220474548216f53f
kati3smi <katismith@csumb.edu>
2026-05-06 11:59:47 -0700

    updated mapping name to match table name

--- src/main/java/com/studyhive/spring_boot_docker/UserController.java
+++ src/main/java/com/studyhive/spring_boot_docker/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserRepository userRepository;
